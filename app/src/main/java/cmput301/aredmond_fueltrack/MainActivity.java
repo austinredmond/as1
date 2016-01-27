ff22e09734b80de50aed3f16d3fbaca7f605f4a7
austinredmond <austinredmond@gmail.com>
2016-01-26 23:09:00 -0700

    Made changes to MainActivity. Now has save and load functionality. Also added minimal changes to onCreate and onStart methods.

--- app/src/main/java/cmput301/aredmond_fueltrack/MainActivity.java
+++ app/src/main/java/cmput301/aredmond_fueltrack/MainActivity.java
@@ -1,8 +1,24 @@
 package cmput301.aredmond_fueltrack;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /**
  * Created by aredmond on 26/01/16.
@@ -10,16 +26,59 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+    private static final String FILENAME = "file.sav";
+    private ArrayList<LogEntry> logEntries = new ArrayList<LogEntry>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFromFile();
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        loadFromFile();
     }
 
+    public void loadFromFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+
+            //Took from https://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/com/google/gson/Gson.html Jan-20-2016
+            Type listType = new TypeToken<ArrayList<LogEntry>>() {}.getType();
+            logEntries = gson.fromJson(in, listType);
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            logEntries = new ArrayList<LogEntry>();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
+    }
+
+    public void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    Context.MODE_PRIVATE);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+            Gson gson = new Gson();
+            gson.toJson(logEntries, out);
+            out.flush();
+
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
+    }
 
 }
