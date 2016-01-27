e7633f2e01f644f912c4774f119e7a6848e73aa8
austinredmond <austinredmond@gmail.com>
2016-01-26 22:25:19 -0700

    Added load and save functionality for the array of log entries.

--- app/src/main/java/cmput301/aredmond_fueltrack/MainActivity.java
+++ app/src/main/java/cmput301/aredmond_fueltrack/MainActivity.java
@@ -3,6 +3,18 @@ package cmput301.aredmond_fueltrack;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /**
  * Created by aredmond on 26/01/16.
@@ -10,16 +22,43 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+    private static final String FILENAME = "file.sav";
+    private ArrayList<LogEntry> logEntries = new ArrayList<LogEntry>();
+    //private ArrayAdapter<LogEntry> adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFromFile();
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        loadFromFile();
+        //adapter = new ArrayAdapter<LogEntry>(this, R.layout.list_item, logEntries);
     }
 
+    public void loadFromFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+
+            //Took from https://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/com/google/gson/Gson.html Jan-20-2016
+            Type listType = new TypeToken<ArrayList<LogEntry>>() {}.getType();
+            logEntries = gson.fromJson(in, listType);
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            logEntries = new ArrayList<LogEntry>();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
+    }
 
 }
